d60fb6d191303ea0d6e3ad04be30489252ba7401
Hamza <hfetuga@gmail.com>
2017-01-18 20:50:10 +0100

    create file to switch between live and prod

--- config.js
+++ config.js
@@ -1,3 +1,20 @@
 /**
  * Created by Hamza Fetuga on 1/18/2017.
  */
+
+/**
+ * 0 = dev
+ * 1 = prod
+ * @type {number}
+ */
+var APP_MODE = 1;
+var MONGODB_MODE = 1;
+
+module.exports = function () {
+    return {
+        mode: {
+            app : APP_MODE,
+            db : MONGODB_MODE
+        }
+    }
+};

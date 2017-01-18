a22df07c517d463937dcd5eeb94d07814f7ce1e2
Hamza <hfetuga@gmail.com>
2017-01-18 20:48:39 +0100

    get urls from constants.js

--- data/models.js
+++ data/models.js
@@ -2,17 +2,14 @@ var mongoose = require('mongoose');
 var appSchema = require('./app');
 var recordSchema = require('./record');
 var dotenv = require('dotenv');
+var constants = require('../data/constants');
 
 module.exports = function () {
 
-    var username = process.env.DEVSTORE_DB_USER;
-    var password = process.env.DEVSTORE_DB_PASS;
 
-    var connectionString = 'mongodb://' + username + ':' + password + '@ds157288.mlab.com:57288/devstore';
+    var connectionString = constants().connectionStrings.useConnectionString;
     var connection = mongoose.createConnection(connectionString);
-
-    // console.log(process.env.DEVSTORE_DB_USER);
-    // console.log(process.env.DEVSTORE_DB_PASS);
+    
     console.log(connectionString);
 
     var Record = connection.model('Record',
